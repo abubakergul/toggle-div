6fd7f4a272a0824035285e44d5f0b1469506ddc9
abubakergul <abubakergul24@gmail.com>
2023-07-09 14:51:40 +0300

    Initial commt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'toggle-div';
+  display = true;
+
+  toggle($event) {
+    $event.stopPropagation();
+    this.display = !this.display;
+  }
+  @HostListener('document:click', ['$event']) onDocumentClick(event) {
+    this.display = false;
+  }
 }
